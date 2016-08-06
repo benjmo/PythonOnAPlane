0c3dee67b8aaf682ce727be06b1cd0192447182e
Benjamin Mo <b.mo96@hotmail.com>
2016-08-06 12:06:41 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def boilerplate():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
