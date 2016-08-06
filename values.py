5074f48010a9bd3e40fbf0a5d6be584dd444e462
Brontesaurus <bronte.kalebic@gmail.com>
2016-08-06 21:55:53 +1000

    added image files and references to them in values.py

--- values.py
+++ values.py
@@ -29,57 +29,68 @@ drinks = [
         'name' : 'Coca-Cola',
         'description' : 'A refreshing drink to '
             + 'soak your tastebuds.',
-        'category' : 'Soft Drink'
+        'category' : 'Soft Drink',
+        'img_name' : 'coke.jpg'
     },
     {
         'name' : 'Sprite',
         'description' : 'Lemonade.',
-        'category' : 'Soft Drink'
+        'category' : 'Soft Drink',
+        'img_name' : 'sprite.jpg'
     },
     {
         'name' : 'Fanta',
         'description' : 'An orange drink.',
-        'category' : 'Soft Drink'
+        'category' : 'Soft Drink',
+        'img_name' : 'fanta.jpg'
     },
     {
         'name' : 'Orange Juice',
         'description' : 'Freshly squeezed oranges.',
-        'category' : 'Juices'
+        'category' : 'Juices',
+        'img_name' : 'orange_juice.jpg'
     },
     {
         'name' : 'Apple Juice',
         'description' : 'Freshly juiced apples.',
-        'category' : 'Juices'
+        'category' : 'Juices',
+        'img_name' : 'apple_juice.jpg'
     },
     {
         'name' : 'English Breakfast',
         'description' : 'Nice morning tea.',
-        'category' : 'Coffee and Tea'
+        'category' : 'Coffee and Tea',
+        'img_name' : 'english_breakfast.jpg'
     },
     {
         'name' : 'Black Coffee',
         'description' : 'Strong coffee.',
-        'category' : 'Coffee and Tea'
+        'category' : 'Coffee and Tea',
+        'img_name' : 'coffee.jpg'
     },
     {
         'name' : 'Red Wine',
         'description' : 'Australian Cab Sav.',
-        'category' : 'Alcoholic Beverages'
+        'category' : 'Alcoholic Beverages',
+        'img_name' : 'red_wine.jpg'
     },
     {
         'name' : 'White Wine',
         'description' : 'Hunter Valley Pinot Gris.',
-        'category' : 'Alcoholic Beverages'
+        'category' : 'Alcoholic Beverages',
+        'img_name' : 'coke.jpg'
     },
     {
         'name' : 'Beer',
         'description' : 'Refreshing Beer.',
-        'category' : 'Alcoholic Beverages'
+        'category' : 'Alcoholic Beverages',
+        'img_name' : 'beer.jpg'
     },
     {
         'name' : 'Rum and Coke',
         'description' : 'Nice drink.',
-        'category' : 'Alcoholic Beverages'
+        'category' : 'Alcoholic Beverages',
+        'img_name' : 'rum_and_coke.jpg'
     }
 ]
 
